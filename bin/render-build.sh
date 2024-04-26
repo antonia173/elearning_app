5991a35d00081c2322fa0b83762c77278bbd0986
antonia173 <antonia.blazevic173@gmail.com>
2024-04-26 18:02:23 +0200

    deployment fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,9 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
+# Check if the admin user exists
+if ! bundle exec rails runner "User.exists?(email: ENV['ADMIN_EMAIL'])"
+then
+  bundle exec rails runner "User.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD], admin: true)"
+fi
