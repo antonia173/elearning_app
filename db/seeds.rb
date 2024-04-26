4fc54b2ced1f0a49126f63b4b1316e99066d21f6
antonia173 <antonia.blazevic173@gmail.com>
2024-04-26 18:51:41 +0200

    deploy fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.production?
+  User.create!(first_name: "Admin", last_name: "User", email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'], admin: true)
+end
